3c9f180a8e57e64372503aa130c97d470b3bc751
charizzo <chazzo96@yahoo.com>
2018-08-29 20:52:22 -0400

    Got the shift cipher working correctly

--- ex1decoder.py
+++ ex1decoder.py
@@ -1,4 +1,72 @@
 #!/usr/bin/env python3
 
+from sys import argv, exit
 
 
+def main():
+	if len(argv) != 3:
+		print("Usage: ./ex1decoder.py encodedFile freqFile")
+		raise Exception("Bad Arguments")
+	
+	fin = open(argv[1], "r")
+	text = fin.read().strip()
+	fin.close()
+
+	fin = open(argv[2], "r")
+	frequencies = {}
+	while 1:
+		text2 = fin.readline().strip()
+		if text2 == '':
+			break
+		line = text2.split(",")
+		frequencies[line[0]] = line[1]
+
+	correctKey = shift_cipher(frequencies, text)
+	print("The correct key is %d" %(correctKey))
+	return 0
+
+def shift_cipher(frequencies, encodedText):
+	decodedText = ''
+	decryptedFreqs = {}
+	currentMin = 100000
+	target = 0.066
+	savedKey = -1
+	for i in range(0,26):
+		for letter in list(frequencies.keys()):
+			decryptedFreqs[letter] = 0
+		for j in encodedText:
+			char = ord(j) - i
+			if char < 97:
+				char += 26
+			
+			decryptedFreqs[chr(char)] = 1 + decryptedFreqs[chr(char)]
+			decodedText += chr(char)
+		
+		sOs = calculateFrequencies(frequencies,decryptedFreqs, len(encodedText))
+		
+		if abs(sOs - target) < currentMin:
+			writeDecodedToFile(decodedText)
+			savedKey = i
+			currentMin = abs(sOs - target)
+		print("Key %d yields differential %f" %(i, abs(sOs - target)))
+		decodedText = ''
+	
+	return savedKey
+
+def calculateFrequencies(frequencies, decryptedFreqs, length):
+	freqs = list(frequencies.values())
+	dFreqs = list(decryptedFreqs.values())
+	
+	sumOfSquares = 0
+	for i in range(0, len(freqs)):
+		sumOfSquares += float(freqs[i]) * (float(dFreqs[i]) / length)
+
+	return sumOfSquares
+
+def writeDecodedToFile(text):
+	fout = open("decodedOutput.txt", "w")
+	fout.write(text)
+	fout.close()
+
+if __name__ == "__main__":
+	exit(main())
