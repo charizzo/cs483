13312d63d88ee606061471cebea7f01525c104d2
charizzo <chazzo96@yahoo.com>
2018-09-16 15:08:59 -0400

    Adding test files.

--- lab1/cbc-enc.py
+++ lab1/cbc-enc.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
+from sys import argv,exit
+from Crypto.Cipher import AES
 
+def main():
+	if len(argv) != 7:
+		print("Usage: ./cbc-enc.py -k <key file> -i <input file> -o <output file>")
+		raise Exception("Bad Command Line Args")
+
+	
+
+
+
+
+if __name__ == "__main__":
+	exit(main())
