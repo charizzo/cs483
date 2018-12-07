a26a39f209577a17c8a1e2fa87b4c90a3295780b
Jiajia Zhao <jiajiazhao@MacBook-Pro-2.local>
2018-12-06 20:01:57 -0500

    testing scripts

---
+++ lab3/testing.sh
@@ -0,0 +1,37 @@
+
+TEST()
+{
+	dir="$1"
+	tmpdir=sdbajsd
+
+	./keygen -t rsa -r grace -pub rsaPub -priv rsaPriv
+	./keygen -r ec -r grace -pub ecPub -priv ecPriv 
+
+	cp -r "$dir" $tmpdir
+	
+	./lock -d "$dir" -p rsaPub -r ecPriv -s grace
+	
+	output=`diff -rq "$dir" $tmpdir`
+	
+	if [ -z "$output" ]; then 
+		echo "lock did not work"
+	fi
+	./unlock -d "$dir" -p ecPub -r rsaPriv -s grace
+	
+	output=`diff -rq "$dir" $tmpdir`
+
+	if [ ! -z "$output" ]; then
+		echo "dir did not match"
+		# exit
+	fi
+
+	rm -rf $tmpdir rsaPub rsaPriv ecPub ecPriv
+}
+
+# TEST "out_most/Lecture note"
+
+path=$1
+for dir in $path/*; do 
+	echo "$dir"
+	TEST "$dir" sdbajsd
+done
